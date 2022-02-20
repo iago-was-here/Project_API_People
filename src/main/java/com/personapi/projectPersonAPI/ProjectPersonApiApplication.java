016be0f2a71df34e1363514921da49f5d350cd9f
iago-was-here <iagoananias587@gmail.com>
2022-02-20 11:54:43 -0300

    initial commit

---
+++ src/main/java/com/personapi/projectPersonAPI/ProjectPersonApiApplication.java
@@ -0,0 +1,13 @@
+package com.personapi.projectPersonAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPersonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPersonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personapi/projectPersonAPI/ProjectPersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personapi.projectPersonAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
